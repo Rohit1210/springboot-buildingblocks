0ba6ee7ce37c100caa6758c075ebccf504b6f5db
rohittiwari <rohit121098@gmail.com>
2026-09-17 23:05:07 +0530

    JsonView first commit

--- src/main/java/com/stacksimplify/restservices/Entities/Client.java
+++ src/main/java/com/stacksimplify/restservices/Entities/Client.java
@@ -3,6 +3,7 @@ package com.stacksimplify.restservices.Entities;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.hateoas.RepresentationModel;
@@ -11,34 +12,42 @@ import java.util.List;
 
 @Entity
 @Table(name = "Client")
-//@JsonIgnoreProperties({"firstname", "lastname"})
-@JsonFilter("userfilter")
+//@JsonIgnoreProperties({"firstname", "lastname"}) - Static Filtering @JsonIgnore
+//@JsonFilter("userfilter") - Used for MappingJacksonValue filtering section
 public class Client extends RepresentationModel<Client> {
 
     @Id
     @GeneratedValue
+    @JsonView(Views.External.class)
     private Long userid;
 
     @OneToMany(mappedBy = "client")
+    @JsonView(Views.Internal.class)
     private List<Order> order;
 
     @NotNull
     @Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+    @JsonView(Views.External.class)
     private String username;
 
     @Column(name = "FIRST_NAME", length = 50, nullable = false)
+    @JsonView(Views.External.class)
     private String firstname;
 
     @Column(name = "LAST_NAME", length = 50, nullable = false)
+    @JsonView(Views.External.class)
     private String lastname;
 
     @Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
+    @JsonView(Views.External.class)
     private String email;
 
     @Column(name = "ROLE", length = 50, nullable = false)
+    @JsonView(Views.Internal.class)
     private String role;
 
     @Column(name = "SSN", length = 50, nullable = false, unique = true)
+    @JsonView(Views.Internal.class)
     //@JsonIgnore
     private String ssn;
 
